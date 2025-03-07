2531931e0bd748cf0d1c1ac787e96551fe0056ce
vignesh80980 <vigneshvicky80980@gmail.com>
2025-03-07 15:08:42 +0530

    Added js

---
+++ product.js
@@ -0,0 +1,30 @@
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("close-nav")
+menuicon.addEventListener("click", function () {
+    sidenav.style.right = 0
+})
+
+closenav.addEventListener("click", function () {
+    sidenav.style.right = "-50%"
+})
+
+var productcontainer = document.getElementById("product-container")
+var search = document.getElementById("search")
+var productlist=productcontainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function()
+{
+    var entervalue= event.target.value.toUpperCase()
+    for(count=0;count<productlist.length;count=count+1)
+    {
+        var productname=productlist[count].querySelector("h1").textContent
+        if(productname.toUpperCase().indexOf(entervalue)<0)
+        {
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+    }
+})
